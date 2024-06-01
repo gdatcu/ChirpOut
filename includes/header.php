a7dcd72d3bcb6822b462360c4300ee353d610c30
Datcu <gdatcu@luxoft.com>
2024-06-01 21:16:39 +0300

    Adding bootstrap and header files

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<?php
+require 'config/config.php';
+
+if (isset($_SESSION['username'])) {
+    $userLoggedIn = $_SESSION['username'];
+}
+else {
+    header("Location: register.php");
+}
+
+?>
+<html>
+    <head>
+        <title>Welcome to ChirpOut</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+    </head>
+    <body>
+
+<!-- Single button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Action <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
+  </ul>
+</div>
