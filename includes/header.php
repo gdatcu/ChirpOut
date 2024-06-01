5f87c4a024b88ab93b76307552e772160536eeb2
Datcu <gdatcu@luxoft.com>
2024-06-01 22:24:33 +0300

    Changing the bootstrap version

--- includes/header.php
+++ includes/header.php
@@ -20,7 +20,7 @@ else {
 
 <!-- Single button -->
 <div class="btn-group">
-  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
     Action <span class="caret"></span>
   </button>
   <ul class="dropdown-menu">
