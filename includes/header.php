b77d1a0294436476d8e28877b565dd078c1765a0
Datcu <gdatcu@luxoft.com>
2024-06-29 12:06:01 +0300

    Adding changes for index, header, styles, htaccess files

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,8 @@ require 'config/config.php';
 
 if (isset($_SESSION['username'])) {
     $userLoggedIn = $_SESSION['username'];
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedIn'");
+    $user = mysqli_fetch_array($user_details_query);
 }
 else {
     header("Location: register.php");
@@ -17,6 +19,7 @@ else {
         <script src="assets/js/bootstrap.js"></script>
 
         <!-- CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 
@@ -31,10 +34,27 @@ else {
         </div>
 
         <nav>
-          <a href="#">Home</a>
-          <a href="#">messages</a>
-          <a href="#">settings</a>
+        <a href="<?php echo $userLoggedIn; ?>">
+          <?php echo $user['first_name'];?>
+          </a>
+          <a href="index.php">
+          <i class="fa fa-home fa-lg"></i>
+          </a>
+          <a href="#">
+          <i class="fa fa-envelope fa-lg"></i>
+          </a>
+          <a href="#">
+          <i class="fa fa-bell-o fa-lg"></i>
+          </a>
+          <a href="#">
+          <i class="fa fa-users fa-lg"></i>
+          </a>
+          <a href="#">
+          <i class="fa fa-cog fa-lg"></i>
+          </a>
 
         </nav>
 
       </div>
+
+      <div class="wrapper">
