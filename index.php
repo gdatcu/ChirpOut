256eae4a81c901e0c27797849c6cb712786cd0c9
Datcu <gdatcu@luxoft.com>
2024-06-30 19:40:55 +0300

    Adding changes for Loading post from database, styling the posts and work on infinite scrolling system

--- index.php
+++ index.php
@@ -36,14 +36,65 @@ if(isset($_POST['post'])) {
 
       </form>
 
-      <?php
-      
-      $user_obj = new User($con, $userLoggedIn);
-      echo $user_obj->getFirstAndLastName();
-
-      ?>
+      <div class="posts_area"></div>
+      <img id="loading" src="assets/images/icons/loading.gif">
 
    </div>
+
+   <script>
+      var userLoggedIn = '<?php echo $userLoggedIn; ?>'
+
+      $(document).ready(function() {
+      $('#loading').show();
+
+         // Original ajax request for loading first posts
+         $.ajax({
+            url: "includes/handlers/ajax_load_posts.php",
+            type: "POST",
+            data: "page=1&userLoggedIn=" + userLoggedIn,
+            cache: false,
+
+            success: function(data) {
+               $('#loading').hide();
+               $('.posts_area').html(data);         
+            
+            }
+         });
+
+         $(window).scroll(function() {
+            var height = $('.posts_area').height(); // div containing posts
+            var scroll_top = $(this).scrollTop();
+            var page = $('.posts_area').find('.nextPage').val();
+            var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+            if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+               $('#loading').show();
+               alert("Hello!");
+               
+               var ajaxReq = $.ajax({
+                  url: "includes/handlers/ajax_load_posts.php",
+                  type: "POST",
+                  data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+                  cache: false,
+
+                  success: function(response) {
+                     $('.posts_area').find('.nextPage').remove(); //Removes current .nextPage
+                     $('.posts_area').find('.noMorePosts').remove();
+
+                     $('#loading').hide();
+                     $('.posts_area').append(response);         
+               
+                  }
+               });
+               
+            } // end if
+
+            return false;
+
+         }); // end (window).scroll(function())
+      });
+
+   </script>
 
 
 </div>
