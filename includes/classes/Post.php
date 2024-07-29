028658a409698ea4a3be388dc6c5565eae450b62
Datcu <gdatcu@luxoft.com>
2024-07-29 11:16:13 +0300

    Adding comments iframe + others.

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -27,7 +27,7 @@ class Post {
 			}
 
 			//insert post 
-			$query = mysqli_query($this->con, "INSERT INTO posts VALUES(default, '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
+			$query = mysqli_query($this->con, "INSERT INTO posts VALUES('', '$body', '$added_by', '$user_to', '$date_added', 'no', 'no', '0')");
 			$returned_id = mysqli_insert_id($this->con);
 
 			//Insert notification 
@@ -83,8 +83,7 @@ class Post {
 				}
 
 				$user_logged_obj = new User($this->con, $userLoggedIn);
-				if($user_logged_obj->isFriend($added_by)) {
-
+				if($user_logged_obj->isFriend($added_by)){
 
 					if($num_iterations++ < $start)
 						continue; 
@@ -104,6 +103,19 @@ class Post {
 					$last_name = $user_row['last_name'];
 					$profile_pic = $user_row['profile_pic'];
 
+					?>
+					<script>
+						function toggle<?php echo $id; ?>() {
+							var element = document.getElementById("toggleComment<?php echo $id; ?>");
+
+							if(element.style.display == "block")
+								element.style.display = "none";
+							else
+								element.style.display = "block";
+
+						}
+					</script>
+					<?php
 
 					//Timeframe
 					$date_time_now = date("Y-m-d H:i:s");
@@ -169,7 +181,7 @@ class Post {
 						}
 					}
 
-					$str .= "<div class='status_post'>
+					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
 								<div class='post_profile_pic'>
 									<img src='$profile_pic' width='50'>
 								</div>
@@ -183,9 +195,11 @@ class Post {
 								</div>
 
 							</div>
+							<div class='post_comment' id='toggleComment$id' style='display:none;'>
+								<iframe src='comment_frame.php?post_id=$id' id='comment_iframe'></iframe>
+							</div>
 							<hr>";
 				}
-				
 
 			} //End while loop
 
