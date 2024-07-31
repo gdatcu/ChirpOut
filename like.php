cabec2c85c774dad7040dd635e497ab45ddeacfc
gdatcu <datcugeorgecristian@gmail.com>
2024-07-31 22:06:46 +0300

    Adding Like and Unlike buttons + styling

--- like.php
+++ like.php
@@ -4,6 +4,20 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
+    <style type="text/css">
+        * {
+            font-family: Arial, Helvetica, Sans-serif;
+        }
+
+        body {
+            background-color: #ffF;
+        }
+
+        form {
+            position: absolute;
+            top: 0;
+        }
+    </style>
 <?php  
 	require 'config/config.php';
 	include("includes/classes/User.php");
@@ -27,15 +41,33 @@
     $total_likes = $row['likes'];
     $user_liked = $row['added_by'];
 
-    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'")
+    $user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user_liked'");
     $row = mysqli_fetch_array($user_details_query);
+    $total_user_likes = $row['num_likes'];
 
     // Like button
+    if(isset($_POST['like_button'])) {
+        $total_likes++;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'" );
+        $total_user_likes++;
+        $user_likes = mysqli_query($con, "UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked'");
+        $insert_user = mysqli_query($con, " INSERT INTO likes VALUES(default, '$userLoggedIn', '$post_id') ");
+
+        // Insert Notification
+    }
 
     // Unlike button
+    if(isset($_POST['unlike_button'])) {
+        $total_likes--;
+        $query = mysqli_query($con, "UPDATE posts SET likes='$total_likes' WHERE id='$post_id'" );
+        $total_user_likes--;
+        $user_likes = mysqli_query($con, " UPDATE users SET num_likes='$total_user_likes' WHERE username='$user_liked' ");
+        $insert_user = mysqli_query($con, " DELETE FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id' ");
 
+        // Insert Notification
+    }
     // Check for previous likes
-    $check_query = mysqli_query($con, "SELECT * FROM likes WHERE username='$usernameLoggedIn' AND post_id='$post_id'");
+    $check_query = mysqli_query($con, "SELECT * FROM likes WHERE username='$userLoggedIn' AND post_id='$post_id'");
     $num_rows = mysqli_num_rows($check_query);
 
     if($num_rows > 0) {
